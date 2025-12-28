996ab6f122294e15be63efd35c837fb033f65609
Yasmine Cole <yasmine.d.johnson@outlook.com>
2025-12-28 15:46:40 -0500

    Add files via upload
    Add Entity base class

---
+++ Entity.java
@@ -0,0 +1,17 @@
+public class Entity {
+    private String name;
+    private String description;
+
+    public Entity(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
