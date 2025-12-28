90bc24f97e3207ab07d22356f6c6ec83a1e1a375
Yasmine Cole <yasmine.d.johnson@outlook.com>
2025-12-28 15:47:25 -0500

    Add files via upload
    Add Item class

---
+++ Item.java
@@ -0,0 +1,20 @@
+public class Item {
+    private String name;
+    private String description;
+
+    public Item(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDesc() {
+        return description;
+    }
+}
+
+
+
